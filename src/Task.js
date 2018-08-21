11b690e3e516ec3d8076d7df95ce0e274711c125
Paolo Leopardi <leopardipaolo@gmail.com>
2018-08-21 21:43:27 +0200

    Lesson 4

--- src/Task.js
+++ src/Task.js
@@ -1,16 +1,30 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import { Draggable } from "react-beautiful-dnd";
 
 const Container = styled.div`
   border: 1px solid lightgrey;
   border-radius: 2px;
   padding: 8px;
   margin-bottom: 8px;
+  background-color: white;
 `;
 
 class Task extends Component {
   render() {
-    return <Container>{this.props.task.content}</Container>;
+    return (
+      <Draggable draggableId={this.props.task.id} index={this.props.index}>
+        {provided => (
+          <Container
+            innerRef={provided.innerRef}
+            {...provided.draggableProps}
+            {...provided.dragHandleProps}
+          >
+            {this.props.task.content}
+          </Container>
+        )}
+      </Draggable>
+    );
   }
 }
 
