36f451622a94708c5e1d36694f1aa818105e4da9
Paolo Leopardi <leopardipaolo@gmail.com>
2018-08-21 23:56:50 +0200

    Lesson 9

--- src/Column.js
+++ src/Column.js
@@ -7,6 +7,9 @@ const Container = styled.div`
   margin: 8px;
   border: 1px solid lightgrey;
   border-radius: 2px;
+  width: 220px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h3`
@@ -16,6 +19,8 @@ const TaskList = styled.div`
   padding: 8px;
   transition: background-color 0.2s ease;
   background-color: ${props => (props.isDraggingOver ? "skyblue" : "white")};
+  flex-grow: 1;
+  min-height: 100px;
 `;
 
 class Column extends Component {
--- src/initialData.js
+++ src/initialData.js
@@ -10,9 +10,19 @@ const initialData = {
       id: "column-1",
       title: "To do",
       taskIds: ["task-1", "task-2", "task-3", "task-4"]
+    },
+    "column-2": {
+      id: "column-2",
+      title: "In progress",
+      taskIds: []
+    },
+    "column-3": {
+      id: "column-3",
+      title: "Done",
+      taskIds: []
     }
   },
-  columnOrder: ["column-1"]
+  columnOrder: ["column-1", "column-2", "column-3"]
 };
 
 export default initialData;
