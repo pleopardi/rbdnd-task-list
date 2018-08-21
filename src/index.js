b5eb485ce7abc6034effca807c3d7c02d4ac9523
Paolo Leopardi <leopardipaolo@gmail.com>
2018-08-21 22:13:33 +0200

    Lesson 6

--- src/index.js
+++ src/index.js
@@ -8,7 +8,23 @@ import Column from "./Column";
 class App extends Component {
   state = initialData;
 
+  onDragStart = start => {
+    document.body.style.color = "orange";
+    document.body.style.transition = "background-color 0.2s ease";
+  };
+
+  onDragUpdate = update => {
+    const { destination } = update;
+    const opacity = destination
+      ? destination.index / Object.keys(this.state.tasks).length
+      : 0;
+    document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity}`;
+  };
+
   onDragEnd = result => {
+    document.body.style.color = "inherit";
+    document.body.style.backgroundColor = "inherit";
+
     const { destination, source, draggableId } = result;
 
     if (!destination) {
@@ -47,7 +63,11 @@ class App extends Component {
 
   render() {
     return (
-      <DragDropContext onDragEnd={this.onDragEnd}>
+      <DragDropContext
+        onDragStart={this.onDragStart}
+        onDragUpdate={this.onDragUpdate}
+        onDragEnd={this.onDragEnd}
+      >
         {this.state.columnOrder.map(columnId => {
           const column = this.state.columns[columnId];
           const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
